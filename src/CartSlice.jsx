d126e678e09e3dd5da57f5324dc85cbe23a16614
carina1196 <carinachanqingwei@gmail.com>
2024-09-11 00:16:01 +0800

    included functionalities in the app to make it work

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
+    totalQuantity: 0,
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+        state.totalQuantity += 1;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+        state.totalQuantity += 1;
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
+      state.totalQuantity = 0;
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        state.totalQuantity =
+          state.totalQuantity - itemToUpdate.quantity + quantity;
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
